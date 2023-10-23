8077d4130b73ec416e5a2a341f12c0cef8f83b1a
sixtoherrera <sixto.herrera@continental.com>
2023-10-23 01:25:31 -0500

    database added

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,22 +1,27 @@
-from flask import Flask 
-# factory
-def create_app():
-    [ ... ]
+from flask import Flask, request
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
-    # register pet blueprint 
+
+# facotry function
+def create_app():
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:superuser$superuser@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    @app.route('/')
+    def hello():
+        return "Go to /pets!"
+
     from . import pet
     app.register_blueprint(pet.bp)
 
-    # return the app 
-    return app
-
-def create_app(): 
-    app = Flask(__name__)
-
-    @app.route('/')
-    def hello(): 
-        return 'Hello, PetFax!'
+    from . import fact
+    app.register_blueprint(fact.bp)
 
     return app
-
-
